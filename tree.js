55d5c09ef4f1831638a67f216297de39bafe4697
The_Circuit <kristofferomdal@gmail.com>
2018-08-29 10:41:01 +0200

    Description support

--- tree.js
+++ tree.js
@@ -1,4 +1,5 @@
 var baseURL = { api: 'https://api.github.com/repos/TheGrandCircuit/IT2/', raw: 'https://raw.githubusercontent.com/TheGrandCircuit/IT2/master/' }
+var Descriptions = {}
 async function getSha() {
     return new Promise(resolve => {
         var xhr = new XMLHttpRequest()
@@ -30,12 +31,15 @@ function parseTree(rawTree) {
         const element = rawTree.tree[i];
         /**@type {Array} */
         var path = element.path.split('/')
-
+        if(path[path.length-1] == 'DESCRIPTION.TXT'){
+            getDesc(path).then(loadDesc)
+            continue
+        }
         var target = tree
         for (let j = 0; j < path.length - 1; j++) {
             target = target.content[path[j]]
-            target.content = {}
         }
+        element.content = {}
         target.content[path[path.length - 1]] = element
     }
     return tree
@@ -52,24 +56,46 @@ async function loadTree() {
             for (const key in item.content) {
                 const element = item.content[key];
                 var DOMElement = document.createElement('div')
+                DOMElement.classList.add('item')
                 DOMElement.id = element.path
-                
+
                 var DOMElementButton = document.createElement('div')
                 DOMElementButton.classList.add('treeButton')
-                DOMElementButton.innerHTML = element.type == 'tree' ? '&#9658' : '&#9634'
+                DOMElementButton.classList.add('closed')
                 DOMElement.appendChild(DOMElementButton)
-                
+                DOMElementButton.innerHTML = '&#9634'
+                if (element.type == 'tree') {
+                    DOMElementButton.innerHTML = '&#9654'
+                    DOMElementButton.addEventListener('click', e => {
+                        if (e.target.classList.contains('closed')) {
+                            e.path[1].classList.remove('closed')
+                            e.target.classList.remove('closed')
+                            e.target.classList.add('open')
+
+                            e.target.innerHTML = '&#9660'
+
+                        } else if (e.target.classList.contains('open')) {
+                            e.target.classList.remove('open')
+                            e.path[1].classList.add('closed')
+                            e.target.classList.add('closed')
+
+                            e.target.innerHTML = '&#9654'
+                        }
+                    })
+                }
+
                 var DOMElement2 = document.createElement('div')
-                DOMElement2.innerHTML = element.path
-                DOMElement2.classList.add('item')
+                DOMElement2.innerHTML = element.path.split('/').pop()
+                DOMElement2.dataset.path = element.path
+                DOMElement2.classList.add('name')
+                DOMElement2.addEventListener('click', e => {
+                    viewDesc(e.target.dataset.path)
+                })
                 DOMElement.appendChild(DOMElement2)
-                
-                                var DOMElementIndent = document.createElement('div')
-                                DOMElementIndent.classList.add('indent')
-                                DOMElement.appendChild(DOMElementIndent)
-                
+
                 if (element.type == 'tree') {
                     DOMElement.classList.add('tree')
+                    DOMElement.classList.add('closed')
                     parent.appendChild(DOMElement)
                     loadContent(element, DOMElement, indent + 1)
                 }
@@ -85,6 +111,30 @@ async function loadTree() {
         resolve()
     })
 
+}
+
+async function getDesc(path){
+    return new Promise(resolve => {
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', baseURL.raw + path, true)
+        xhr.onload = function () {
+            resolve(this.response)
+        }
+        xhr.send()
+    })
+}
+
+async function loadDesc(text){
+    var descriptions = text.split('#')
+    for (let i = 1; i < descriptions.length; i += 2) {
+        Descriptions[descriptions[i]] = descriptions[i+1].replace('\n','').trim();
+        
+    }
+}
+
+function viewDesc(path){
+    var box = document.getElementById('desc')
+    box.innerHTML = Descriptions[path] || 'This item has no description.'
 }
 
 loadTree()
