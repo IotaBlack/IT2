286b7752e15ea2020b518234c8c35ea537a3cae2
The_Circuit <kristofferomdal@gmail.com>
2018-09-17 10:39:46 +0200

    Sorting

---
+++ Sorting/main.js
@@ -0,0 +1,294 @@
+/**@type {HTMLCanvasElement} */
+var cvs = document.getElementById('cvs')
+var ctx = cvs.getContext('2d')
+
+
+var numItems = 1000
+var maxbit = (numItems - 1).toString(2).length
+var scale = numItems > 1000 ? numItems / 1000 : 1
+var iterationsPerFrame = 10
+var iterationsToDo = 0
+cvs.height = numItems / scale
+cvs.width = numItems / scale
+
+/**@type {Canvas} */
+var dcvs = new Canvas(ctx.getImageData(0, 0, cvs.width, cvs.height))
+
+var list = new Array(numItems)
+
+for (let i = 0; i < list.length; i++) {
+    list[i] = i
+}
+
+function shuffle(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        var rInt = Random(0, arr.length - 1)
+        tmp = arr[i]
+        arr[i] = arr[rInt]
+        arr[rInt] = tmp
+    }
+}
+
+shuffle(list)
+
+
+var sorts = {
+    swap: function (a, i, j) {
+        tmp = a[i]
+        a[i] = a[j]
+        a[j] = tmp
+
+        dcvs.clearRect(i / scale, 0, 1, cvs.height)
+        dcvs.clearRect(j / scale, 0, 1, cvs.height)
+        dcvs.rect(i / scale, cvs.height - a[i] / scale, 1, a[i] / scale)
+        dcvs.rect(j / scale, cvs.height - a[j] / scale, 1, a[j] / scale)
+    },
+    /**@param {Array} arr */
+    bubble: function* (arr) {
+        let index = 0
+        let max = arr.length
+
+        while (max) {
+            for (let i = 0; i < max; i++) {
+                yield { marker: 'red', index: i }
+                if (arr[i] > arr[i + 1]) {
+                    this.swap(arr, i, i + 1)
+                }
+            }
+            max--
+        }
+    },
+
+    /**@param {Array} arr */
+    selection: function* (arr) {
+        var minIdx, temp,
+            len = arr.length;
+        for (var i = 0; i < len; i++) {
+            minIdx = i;
+            yield { marker: 'green', index: i }
+            for (var j = i + 1; j < len; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                    yield { marker: 'green', index: j }
+                }
+                yield { marker: 'red', index: j }
+            }
+            this.swap(list, i, minIdx)
+        }
+        return arr;
+    },
+
+    /**@param {Array} arr */
+    insertion: function* (arr) {
+        var i, len = arr.length, el, j;
+
+        for (i = 1; i < len; i++) {
+            // yield { marker: 'red', index: i }
+            let val = arr[i]
+            j = i
+            while (j > 0 && val < arr[j - 1]) {
+
+                yield { marker: 'red', index: j }
+
+                this.swap(arr, j, j - 1)
+                j--
+            }
+        }
+    },
+
+    /**@param {Array} arr */
+    merge: function* (arr, left, right) {
+        if (left < right) {
+
+            let mid = Math.floor((right-left)/2)+left
+
+            yield* this.merge(arr,left,mid)
+            yield* this.merge(arr,mid+1,right)
+            yield* this.merge.merge(arr,left,mid,right)
+
+
+        }
+    },
+
+    /**@param {Array} arr */
+    quick: function* (arr, left, right) {
+        var pivot
+        var partitionIndex
+
+
+        if (left < right) {
+            //Partition
+            pivot = right;
+            var pivotValue = arr[pivot],
+
+                partitionIndex = left;
+            yield { marker: 'red', index: pivot, value: pivotValue }
+            for (var i = left; i < right; i++) {
+                yield { marker: 'green', index: i }
+                if (arr[i] < pivotValue) {
+                    this.swap(arr, i, partitionIndex)
+
+                    partitionIndex++;
+                    yield { marker: 'blue', index: partitionIndex }
+                }
+            }
+
+            this.swap(arr, right, partitionIndex)
+            //sort left and right
+            yield* this.quick(arr, left, partitionIndex - 1);
+            yield* this.quick(arr, partitionIndex + 1, right);
+        }
+    },
+
+    /**WIP */
+    binaryQuick: function* (arr, left, right, bit) {
+        var partitionIndex
+
+        console.log(left, right)
+        if (left < right) {
+            //Partition
+
+            partitionIndex = left;
+            for (var i = left; i < right; i++) {
+                yield { marker: 'green', index: i }
+                if (!(arr[i] & (1 << maxbit - bit))) {
+                    this.swap(arr, i, partitionIndex)
+
+                    partitionIndex++;
+                    yield { marker: 'blue', index: partitionIndex }
+                }
+            }
+
+            this.swap(arr, right, partitionIndex)
+            //sort left and right
+            yield* this.binaryQuick(arr, left, partitionIndex - 1, bit + 1);
+            yield* this.binaryQuick(arr, partitionIndex + 1, right, bit + 1);
+        }
+    }
+}
+
+sorts.merge.merge = function* (arr, left, mid, right) {
+    let left1 = left
+    let left2 = mid
+    let right1 = mid + 1
+    let right2 = right
+    let result = []
+    while ((left1 <= left2) && (right1 <= right2)) {
+        if (arr[left1] <= arr[right1]) {
+            result.push(arr[left1++])
+            yield { marker: 'red', index: left1 }
+        } else {
+            result.push(arr[right1++])
+            yield { marker: 'red', index: right1 }
+        }
+    }
+
+    while (left1 <= left2) {
+        result.push(arr[left1++])
+        yield { marker: 'red', index: left1 }
+    }
+
+    while (left1 <= left2) {
+        result.push(arr[right1++])
+        yield { marker: 'red', index: right1 }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        arr[left + i] = result[i];
+
+        yield { marker: 'blue', index: left+i }
+    }
+}
+
+//var sort = sorts.bubble(list)
+//var sort = sorts.selection(list)
+//var sort = sorts.insertion(list)
+var sort = sorts.merge(list, 0, list.length-1)
+//var sort = sorts.quick(list, 0, list.length - 1)
+//var sort = sorts.binaryQuick(list, 0, list.length - 1, 1)
+
+
+var markers = {
+    red: { color: [255, 0, 0, 255], index: -1 },
+    green: { color: [0, 255, 0, 255], index: -1 },
+    blue: { color: [0, 0, 255, 255], index: -1 },
+
+}
+function update() {
+    let yieldval, marker
+    clearMarkers()
+    iterationsToDo += iterationsPerFrame
+
+    while (iterationsToDo >= 1) {
+        iterationsToDo--
+        yieldval = sort.next()
+        if (yieldval.value) {
+            marker = yieldval.value.marker
+            markers[marker].index = yieldval.value.index
+        }
+    }
+    Draw()
+    if (!yieldval || !yieldval.done) {
+        drawMarkers()
+        requestAnimationFrame(update)
+    } else {
+        console.log('Done!')
+        clearMarkers()
+    }
+    ctx.putImageData(dcvs.src, 0, 0)
+}
+
+function Draw() {
+    ctx.clearRect(0, 0, cvs.width, cvs.height)
+    dcvs.clearRect(0, 0, cvs.width, cvs.height, [255, 255, 255, 255])
+    for (let i = 0; i < list.length; i++) {
+        ctx.fillRect(i / scale, cvs.height, 1, -list[i] / scale)
+        dcvs.rect(i / scale, cvs.height - list[i] / scale, 1, list[i] / scale)
+    }
+    ctx.putImageData(dcvs.src, 0, 0)
+}
+
+function drawMarkers() {
+    for (const key in markers) {
+        var marker = markers[key];
+
+        dcvs.setColor(marker.color)
+        dcvs.rect(marker.index / scale,
+            cvs.height - list[marker.index] / scale,
+            1,
+            list[marker.index] / scale)
+    }
+    dcvs.setColor([0, 0, 0, 255])
+}
+function clearMarkers() {
+    dcvs.setColor([0, 0, 0, 255])
+    for (const key in markers) {
+        var marker = markers[key];
+        dcvs.rect(marker.index / scale,
+            cvs.height - list[marker.index] / scale,
+            1,
+            list[marker.index] / scale)
+
+    }
+}
+
+
+
+/**
+ * 
+ * @param {Number} max 
+ * @param {Number} min 
+ */
+function Random(min, max) {
+    let range = max - min + 1
+    let value = Math.random() * range + min
+
+    return Math.floor(value)
+}
+
+
+
+
+
+Draw()
+update()
---
+++ simpleGFX.js
@@ -0,0 +1,90 @@
+/**
+ * @typedef {Object} Canvas
+ * @property {ImageData} src
+ * @property {Number} width
+ * @property {Number} height
+ * @property {Number[]} color
+ * @function rect
+ */
+
+class Canvas {
+    /**
+     * @constructor
+     * @param {ImageData} src
+     */
+    constructor(src) {
+        this.src = src
+        this.width = src.width
+        this.heigth = src.height
+        this.color = [0, 0, 0, 255]
+        this.clearColor = [255,255,255,255]
+        this.image = []
+
+        for (let i = 0; i < this.width; i++) {
+            this.image[i] = []
+
+            for (let j = 0; j < this.heigth; j++) {
+                this.image[i][j] = new Uint8Array(src.data.buffer,pointToIndex(i,j,src.width),4)
+                this.image[i][j][3] = 255
+            }
+        }
+
+        return this
+    }
+    /** @param {Number[]} c  */
+    setColor(c) {
+        this.color = c.slice()
+    }
+
+    /** @param {Number} x 
+     *  @param {Number} y 
+     *  @param {Number} w 
+     *  @param {Number} h 
+     */
+    rect(x, y, w, h) {
+        x = Math.floor(x)
+        y = Math.floor(y)
+        w = Math.floor(w)
+        h = Math.floor(h)
+        for (let i = 0; i < w; i++) {
+            for (let j = 0; j < h; j++) {
+                let index = pointToIndex(i + x, j + y, this.width)
+                this.src.data[index + 0] = this.color[0]
+                this.src.data[index + 1] = this.color[1]
+                this.src.data[index + 2] = this.color[2]
+                this.src.data[index + 3] = this.color[3]
+                /**this.image[i+x][j+y][0] = this.color[0]
+                this.image[i+x][j+y][1] = this.color[1]
+                this.image[i+x][j+y][2] = this.color[2]
+                this.image[i+x][j+y][3] = this.color[3]**/
+            }
+        }
+    }
+
+    /** @param {Number} x
+     *  @param {Number} y
+     *  @param {Number} w
+     *  @param {Number} h
+     */
+    clearRect(x, y, w, h,color) {
+        x = Math.floor(x)
+        y = Math.floor(y)
+        w = Math.floor(w)
+        h = Math.floor(h)
+        for (let i = 0; i < w; i++) {
+            for (let j = 0; j < h; j++) {
+                let index = pointToIndex(i + x, j + y, this.width)
+                this.src.data[index + 0] = this.clearColor[0]
+                this.src.data[index + 1] = this.clearColor[1]
+                this.src.data[index + 2] = this.clearColor[2]
+                this.src.data[index + 3] = this.clearColor[3]
+            }
+        }
+    }
+}
+
+
+
+
+
+function pointToIndex(x, y, w) { return (y * w + x) * 4 }
