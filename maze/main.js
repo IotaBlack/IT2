c7caaec7be477707692297a0da8c40e6250a8a79
The_Circuit <kristofferomdal@gmail.com>
2018-10-15 11:20:28 +0200

    custom maze

--- maze/main.js
+++ maze/main.js
@@ -5,14 +5,18 @@ var ctx = cvs.getContext('2d')
 
 var size = { x: 100 + 1, y: 100 + 1 }
 //canvas target size
-var cvsSize = {}
-var scale = {}
-var iterationsPerFrame = 10
+var cvsSize = { x: 700, y: 700 }
+var scale = scale = { x: Math.round(cvsSize.x / size.x), y: Math.round(cvsSize.y / size.y) }
+cvs.style.width = cvs.width = size.x * scale.x
+cvs.style.height = cvs.height = size.y * scale.y
+var speed = 10
+var iterationsPerFrame = speed
 var iterationsToDo = 1
 var absoluteSpeed = true
 var animationFrameID = 0
 /**@type {Canvas} */
 var dcvs = new Canvas(ctx.getImageData(0, 0, cvs.width, cvs.height))
+dcvs.scale = scale
 
 
 
@@ -20,14 +24,15 @@ var Debug = false
 var gridcolors = [[255, 255, 255, 255],
 [0, 0, 0, 255],
 [0, 255, 0, 255]]
-/**@type {Maze}*/
-var maze
+/**@type {Grid}*/
+var maze = new Grid(size.x, size.y, 1, 2)
 /**@type {Generator} */
 var generator
 var generatorStr = "Caver(1, 1, maze, 1, 0)"
 var input = {
     setSpeed: function (value) {
-        iterationsPerFrame = parseFloat(value)
+        speed = parseFloat(value)
+        iterationsPerFrame = speed
     },
     start: function () {
         Setup()
@@ -42,9 +47,7 @@ var input = {
 
 
 function Setup() {
-    //canvas target size
-    cvsSize = { x: 700, y: 700 }
-    scale = { x: Math.ceil(cvsSize.x / size.x), y: Math.ceil(cvsSize.y / size.y) }
+    scale = { x: Math.round(cvsSize.x / size.x), y: Math.round(cvsSize.y / size.y) }
     cvs.style.width = cvs.width = size.x * scale.x
     cvs.style.height = cvs.height = size.y * scale.y
     dcvs = new Canvas(ctx.getImageData(0, 0, cvs.width, cvs.height))
@@ -59,7 +62,8 @@ function Setup() {
 }
 
 function flood() {
-    generator = flooder(1, 1, maze)
+    //generator = flooder(1, 1, size.x - 2, size.y - 2, maze)
+    generator = A_Star(1, 1, size.x - 2, size.y - 2, maze, 0, 2, 3, 1)
     Tick()
 }
 
@@ -85,11 +89,51 @@ function Tick() {
     }
 }
 
+cvs.addEventListener('mousemove', e => {
+
+    if (!(e.buttons & 3)) { return }
+
+    var pos = { x: Math.floor(e.offsetX / scale.x), y: Math.floor(e.offsetY / scale.y) }
+    var val = 0
+    if (e.buttons == 1) {
+
+        dcvs.setColor([0, 0, 0, 255])
+
+        val = 1
+    } else if (e.buttons == 2) {
+        dcvs.setColor([255, 255, 255, 255])
+
+        val = 0
+    }
+
+    var offsets = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 1 }]
+
+    for (let i = 0; i < offsets.length; i++) {
+        const offset = offsets[i];
+        dcvs.rect(pos.x + offset.x, pos.y + offset.y, 1, 1)
+
+        maze.grid[pos.x + offset.x][pos.y + offset.y] = val
+
+    }
+
+    ctx.putImageData(dcvs.src, 0, 0)
+})
+
+
 function Grid(x, y, init, distance) {
     this.grid = []
     this.size = { x: x, y: y }
     this.distance = distance || 2
-    this.directiondefinitions = { left: { x: -1, y: 0 }, right: { x: +1, y: 0 }, up: { x: 0, y: -1 }, down: { x: 0, y: +1 } }
+    this.directiondefinitions = {
+        north: { x: 0, y: -1 },
+        south: { x: 0, y: +1 },
+        east: { x: +1, y: 0 },
+        west: { x: -1, y: 0 },
+        northEast: { x: +1, y: -1 },
+        northWest: { x: -1, y: -1 },
+        southEast: { x: +1, y: +1 },
+        southWest: { x: -1, y: +1 }
+    }
 
     for (i = 0; i < x; i++) {
         this.grid[i] = []
@@ -107,23 +151,46 @@ Grid.prototype.fill = function (x, y, w, h, v) {
     }
 }
 
-Grid.prototype.getNeighborDirections = function (x, y, values, d) {
+maze.fill(1, 1, size.x - 1, size.y - 1, 0)
+Draw()
+
+
+/**
+ * @param {Number} x X coord to check from
+ * @param {Number} y Y coord to check from
+ * @param {Number} [d] Distance from point to check from
+ * @param {function} isAllowed 
+ * @param {Boolean} [diagonal] 
+ * @param {Boolean} [advanced] 
+ * @returns {{x:Number,y:Number}[]} An array of the valid directions
+ */
+Grid.prototype.getNeighborDirections = function (x, y, isAllowed, d, diagonal, advanced) {
     d = d || this.distance
     var directions = []
-    if (this.grid[x - d] && values.includes(this.grid[x - d][y])) { directions.push(this.directiondefinitions.left) }
-    if (this.grid[x + d] && values.includes(this.grid[x + d][y])) { directions.push(this.directiondefinitions.right) }
-    if (values.includes(this.grid[x][y - d])) { directions.push(this.directiondefinitions.up) }
-    if (values.includes(this.grid[x][y + d])) { directions.push(this.directiondefinitions.down) }
-
-
+    if (this.grid[x - d] && isAllowed(this.grid[x - d][y])) { directions.push(this.directiondefinitions.west) }
+    if (this.grid[x + d] && isAllowed(this.grid[x + d][y])) { directions.push(this.directiondefinitions.east) }
+    if (isAllowed(this.grid[x][y - d])) { directions.push(this.directiondefinitions.north) }
+    if (isAllowed(this.grid[x][y + d])) { directions.push(this.directiondefinitions.south) }
+    if (diagonal) {
+        if (advanced) {//Dont cut corners
+            if (this.grid[x - d] && isAllowed(this.grid[x][y - d]) && isAllowed(this.grid[x - d][y]) && isAllowed(this.grid[x - d][y - d])) { directions.push(this.directiondefinitions.northWest) }
+            if (this.grid[x + d] && isAllowed(this.grid[x][y - d]) && isAllowed(this.grid[x + d][y]) && isAllowed(this.grid[x + d][y - d])) { directions.push(this.directiondefinitions.northEast) }
+            if (this.grid[x - d] && isAllowed(this.grid[x][y + d]) && isAllowed(this.grid[x - d][y]) && isAllowed(this.grid[x - d][y + d])) { directions.push(this.directiondefinitions.southWest) }
+            if (this.grid[x + d] && isAllowed(this.grid[x][y + d]) && isAllowed(this.grid[x + d][y]) && isAllowed(this.grid[x + d][y + d])) { directions.push(this.directiondefinitions.southEast) }
+        } else {//Cut corners
+            if (this.grid[x - d] && isAllowed(this.grid[x - d][y - d])) { directions.push(this.directiondefinitions.northWest) }
+            if (this.grid[x + d] && isAllowed(this.grid[x + d][y - d])) { directions.push(this.directiondefinitions.northEast) }
+            if (this.grid[x - d] && isAllowed(this.grid[x - d][y + d])) { directions.push(this.directiondefinitions.southWest) }
+            if (this.grid[x + d] && isAllowed(this.grid[x + d][y + d])) { directions.push(this.directiondefinitions.southEast) }
+        }
+    }
     return directions
 }
 
 /**
- * 
  * @param {Number} x 
  * @param {Number} y 
- * @param {Maze} maze 
+ * @param {Grid} maze 
  * @param {Number} distance 
  * @param {Number} target 
  * @param {Number} trace 
@@ -140,7 +207,7 @@ function* Caver(x, y, maze, target, trace, distance) {
     var directions = []
     while (true) {
         yield
-        directions = maze.getNeighborDirections(pos.x, pos.y, [target], distance)
+        directions = maze.getNeighborDirections(pos.x, pos.y, v => v == target, distance)
         if (directions.length != 0) {
             direction = directions[Math.floor(Math.random() * directions.length)]
 
@@ -168,10 +235,9 @@ function* Caver(x, y, maze, target, trace, distance) {
 
 
 /**
- * 
  * @param {Number} x 
  * @param {Number} y 
- * @param {Maze} maze 
+ * @param {Grid} maze 
  */
 function* Prim(x, y, maze, target, trace, distance) {
     pos = { x: x, y: y }
@@ -193,7 +259,7 @@ function* Prim(x, y, maze, target, trace, distance) {
         dcvs.rect(pos.x, pos.y, 1, 1)
         maze.grid[pos.x][pos.y] = trace
 
-        directions = maze.getNeighborDirections(pos.x, pos.y, [trace], distance)
+        directions = maze.getNeighborDirections(pos.x, pos.y, v => v == trace, distance)
 
         var direction = directions[Random(0, directions.length - 1)]
         var pos2 = Object.assign({}, pos)
@@ -208,7 +274,7 @@ function* Prim(x, y, maze, target, trace, distance) {
 
         }
 
-        directions = maze.getNeighborDirections(pos.x, pos.y, [target], distance)
+        directions = maze.getNeighborDirections(pos.x, pos.y, v => v == target, distance)
         for (let i = 0; i < directions.length; i++) {
             direction = directions[i]
             var cell = { x: pos.x + direction.x * distance, y: pos.y + direction.y * distance }
@@ -219,8 +285,7 @@ function* Prim(x, y, maze, target, trace, distance) {
 }
 
 /**
- * 
- * @param {Maze} maze 
+ * @param {Grid} maze 
  */
 function* Divider(maze, x1, y1, x2, y2, roomval = 0, wallval = 1, divideFuncWall = Random, divideFuncHole = Random) {
     if (x2 - x1 < 1 || y2 - y1 < 2) { return }
@@ -278,84 +343,208 @@ function* Divider(maze, x1, y1, x2, y2, roomval = 0, wallval = 1, divideFuncWall
  * 
  * @param {Number} x 
  * @param {Number} y 
- * @param {Maze} maze 
+ * @param {Number} x2 
+ * @param {Number} y2 
+ * @param {Grid} maze 
+ * @param {Number} target 
+ * @param {Number} trace 
  */
-function* flooder(x, y, maze, target, trace) {
+function* flooder(x, y, x2, y2, maze, target, trace) {
     var pos = { x: x, y: y }
     var toExplore = [{ x: x, y: y }]
     target = target || 0
     trace = trace || 2
 
-
+    var pathGrid = new Grid(maze.size.x, maze.size.y, null, 1)
+    pathGrid.grid[x][y] = true
     var directions = []
     while (true) {
         yield
-        if (absoluteSpeed) { iterationsPerFrame = toExplore.length }
+        if (absoluteSpeed) { iterationsPerFrame = Math.max(1, toExplore.length) * speed }
         var pos = toExplore.shift()
-        if (!pos) { return }
+        if (!pos) { break }
         dcvs.setColor([0, 0, 255, 255])
         dcvs.rect(pos.x, pos.y, 1, 1)
         maze.grid[pos.x][pos.y] = trace
 
-        directions = maze.getNeighborDirections(pos.x, pos.y, [target], 1)
+        directions = maze.getNeighborDirections(pos.x, pos.y, v => v == target, 1, true, true)
         for (let i = 0; i < directions.length; i++) {
-            direction = directions[i]
+            var direction = directions[i]
             dcvs.setColor([255, 0, 0, 255])
             dcvs.rect((pos.x + direction.x), (pos.y + direction.y), 1, 1)
             maze.grid[pos.x + direction.x][pos.y + direction.y] = trace
             toExplore.push({ x: pos.x + direction.x, y: pos.y + direction.y })
+            pathGrid.grid[pos.x + direction.x][pos.y + direction.y] = { x: pos.x, y: pos.y }
         }
+    }
+
+    yield* reconstructPath(x2, y2, pathGrid.grid)
+}
+
+
+/**
+ * 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Number} x2 
+ * @param {Number} y2 
+ * @param {Grid} maze 
+ * @param {Number} target 
+ * @param {Number} trace 
+ * @param {Number} trace2 
+ * @param {Number} g
+ */
+function* A_Star(x, y, x2, y2, maze, target, trace, trace2, g) {
+    var cell = { x: x, y: y }
+    var openSet = [{ x: x, y: y, gCost: 0, hCost: heuristic(x, y, x2, y2) }]
+    target = target || 0
+    trace = trace || 2
+
+    function heuristic(x, y, x2, y2) {
+        var dx = Math.abs(x2 - x)
+        var dy = Math.abs(y2 - y)
+        //return dx + dy //manhattan
+        return dx + dy - Math.min(dx,dy) //diagonal
+        //return Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y))  //euclidean
+    }
+
+    var pathGrid = new Grid(maze.size.x, maze.size.y, null, 1)
+    pathGrid.grid[x][y] = true
+    //distance from start
+    var GGrid = new Grid(maze.size.x, maze.size.y, null, 1)
+    GGrid.grid[x][y] = 0
+    var HGrid = new Grid(maze.size.x, maze.size.y, null, 1)
+    HGrid.grid[x][y] = heuristic(x, y, x2, y2)
+    var directions = []
+    var openSetGrid = []
+    for (let i = 0; i < maze.grid.length; i++) {
+        openSetGrid[i] = []
+    }
+
+    while (true) {
+        yield
+        var min = 0
+        for (let i = 0; i < openSet.length; i++) {
+            const cell = openSet[i];
+            min = (openSet[min].fcost > cell.fcost) ? i : min
+        }
+        var cell = openSet.splice(min, 1)[0]
+        if (!cell || (cell.x == x2 && cell.y == y2)) { break }
+        dcvs.setColor([0, 0, 255, 255])
+        dcvs.rect(cell.x, cell.y, 1, 1)
+        maze.grid[cell.x][cell.y] = trace
+
+        directions = maze.getNeighborDirections(cell.x, cell.y, v => v == target || v == trace2, 1, true)
+        for (let i = 0; i < directions.length; i++) {
+            var direction = directions[i]
+            dcvs.setColor([255, 0, 0, 255])
+            dcvs.rect((cell.x + direction.x), (cell.y + direction.y), 1, 1)
+            var ncell = { x: cell.x + direction.x, y: cell.y + direction.y }
+            var gCost = cell.gCost + ((direction.x != 0 && direction.y != 0) ? g * 1.4 : g)
+            var hCost = heuristic(cell.x + direction.x, cell.y + direction.y, x2, y2)
+            ncell.gCost = gCost
+            ncell.hCost = hCost
+            ncell.fcost = gCost + hCost
+            var index = 0
+            if (maze.grid[ncell.x][ncell.y] == trace2) {
+                if (openSetGrid[ncell.x][ncell.y].fcost > ncell.fcost) {
+                    var ocell = openSetGrid[ncell.x][ncell.y]
+
+                    ocell.gCost = ncell.gCost
+                    ocell.hCost = ncell.hCost
+                    ocell.fcost = ncell.fcost
+
+                    pathGrid.grid[ncell.x][ncell.y] = { x: cell.x, y: cell.y }
+                }
+
+                continue
+            }
+            openSetGrid[ncell.x][ncell.y] = ncell
+            pathGrid.grid[ncell.x][ncell.y] = { x: cell.x, y: cell.y }
+            openSet.push(ncell)
+            maze.grid[ncell.x][ncell.y] = trace2
+
+        }
+    }
+
+
+    yield* reconstructPath(x2, y2, pathGrid.grid)
+}
+
+function* reconstructPath(x, y, grid) {
+    var path = []
+    pos = { x: x, y: y }
+    while (true) {
+        yield
+        dcvs.setColor([0, 255, 0, 255])
+        dcvs.rect(pos.x, pos.y, 1, 1)
+        path.push(pos)
+        pos = grid[pos.x][pos.y]
+        if (pos == true) { break }
     }
 }
 
 /**
  * 
- * @param {Maze} maze 
+ * @param {Grid} maze 
  */
 function saveMaze(maze) {
     /**@type {Number[]}*/
     var maze1D = maze.grid.flat()
 
-    var mazeArray = []
 
-    var byte = 0
+    var mazeStr = maze.size.x + ':' + maze.size.y + ';'
+
     var byteStr = ''
     while (maze1D.length) {
         var cell = maze1D.pop()
         byteStr += cell
-        if (byteStr.length == 32) {
-            var num = parseInt(byteStr, 2)
-            mazeArray[byte++] = num
+        if (byteStr.length == 4) {
+            var num = parseInt(byteStr, 2).toString(16)
+            mazeStr += num
             byteStr = ''
         }
     }
-    var num = parseInt(byteStr, 2)
-    mazeArray[byte++] = num
+    var num = parseInt(byteStr, 2).toString(16)
+    mazeStr += num
     byteStr = ''
 
-    return { width: maze.grid.length, height: maze.grid[0].length, src: mazeArray }
+    return mazeStr
 }
 
-function loadMaze(maze) {
-    var rawString = ''
+/**
+ * 
+ * @param {String} mazeStr 
+ */
+function loadMaze(mazeStr) {
     var grid = []
-    for (let i = 0; i < maze.src.length - 1; i++) {
-        rawString += maze.src[i].toString(2).padStart(32, '0')
-    }
-    rawString += maze.src[maze.src.length - 1].toString(2).
-        rawString = rawString.substr(0, maze.height * maze.width)
-    var row = 0
-    while (rawString.length) {
-        var segment = rawString.substr(0, maze.width)
-        rawString = rawString.substr(maze.width)
 
-        segment = segment.split('')
-        grid[row] = []
-        let length = segment.length
-        for (let i = 0; i < length; i++) {
-            grid[row][i] = parseInt(segment.shift())
+    var params = mazeStr.split(/[:;]/)
+    var size = { x: params[0], y: params[1] }
+    var hexString = params[2]
+    var binaryString = ''
+    while (hexString.length) {
+        var num = hexString.substr(0, 1)
+        hexString = hexString.substr(1)
+
+        var bits = parseInt(num, 16).toString(2).padStart(4, 0)
+
+        binaryString += bits
+
+    }
+
+    var last4 = binaryString.substr(binaryString.length - 4)
+    binaryString = binaryString.substr(0, binaryString.length - 4)
+    last4 = last4.substr(4 - (size.x * size.y) % 4)
+    binaryString += last4
+
+    var binaryArray = binaryString.split('')
+
+    for (let x = 0; x < size.x; x++) {
+        grid[x] = []
+        for (let y = 0; y < size.y; y++) {
+            grid[x][y] = binaryArray.pop()
         }
-        row++
     }
 
     return grid
@@ -372,4 +561,52 @@ function Random(min, max) {
     let value = Math.random() * range + min
 
     return Math.floor(value)
+}
+
+
+function HSVToRGB(h, s, v) {
+
+    var c = v * s
+
+    var x = c * (1 - Math.abs((h / 60) % 2) - 1)
+
+    var m = v - c
+    var rgb = {}
+    if (h < 60) {
+        rgb = { r: c, g: x, b: 0 }
+    } else if (h < 120) {
+        rgb = { r: x, g: c, b: 0 }
+    } else if (h < 180) {
+        rgb = { r: 0, g: c, b: x }
+    } else if (h < 240) {
+        rgb = { r: 0, g: x, b: c }
+    } else if (h < 300) {
+        rgb = { r: x, g: 0, b: c }
+    } else if (h < 360) {
+        rgb = { r: c, g: 0, b: x }
+    }
+
+    var r = Math.ceil((rgb.r + m) * 255)
+    var g = Math.ceil((rgb.g + m) * 255)
+    var b = Math.ceil((rgb.b + m) * 255)
+
+
+    //return {r:r,g:g,b:b}
+    return [r, g, b, 255]
+}
+
+
+function Drawheuristic(x, y) {
+    function heuristic(x, y, x2, y2) {
+        return Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y))
+    }
+
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+            dcvs.setColor(HSVToRGB(heuristic(i, j, x, y), 1, 1))
+            dcvs.rect(i, j, 1, 1)
+        }
+    }
+
+
 }
